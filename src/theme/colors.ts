7085fbd94d0c3ae984d91104f38e8d40a61376e1
Florin <Olaru>
2021-02-26 17:32:41 +0200

    Uploading files

---
+++ src/theme/colors.ts
@@ -0,0 +1,57 @@
+import { Colors } from "./types";
+
+export const baseColors = {
+  failure: "#ED4B9E",
+  primary: "#375254",
+  primaryBright: "#53DEE9",
+  primaryDark: "#0098A1",
+  secondary: "#7645D9",
+  success: "#31D0AA",
+  warning: "#FFB237",
+};
+
+export const brandColors = {
+  binance: "#F0B90B",
+};
+
+export const lightColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  background: "#9cddf9",
+  backgroundDisabled: "#9cddf9",
+  contrast: "#73d2ff",
+  invertedContrast: "#FFFFFF",
+  input: "#eeeaf4",
+  inputSecondary: "#d7caec",
+  tertiary: "#EFF4F5",
+  text: "#000000",
+  textDisabled: "#000000",
+  textSubtle: "#000000",
+  borderColor: "#1e343e",
+  card: "#73d2ff",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
+  },
+};
+
+export const darkColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  secondary: "#9A6AFF",
+  background: "#100C18",
+  backgroundDisabled: "#3c3742",
+  contrast: "#FFFFFF",
+  invertedContrast: "#191326",
+  input: "#483f5a",
+  inputSecondary: "#66578D",
+  primaryDark: "#0098A1",
+  tertiary: "#353547",
+  text: "#73d2ff",
+  textDisabled: "#73d2ff",
+  textSubtle: "#73d2ff",
+  borderColor: "#524B63",
+  card: "#27262c",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
+  },
+};
